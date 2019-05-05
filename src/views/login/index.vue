628ff4e19596aedf0b0ff5a76742965395b3d467
姜斌 <jiangbin@cmstop.com>
2019-05-05 10:04:21 +0800

    tab切换

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -13,10 +13,10 @@
             <div class="login-box">
                 <div class="login-logo"></div>
                 <ul class="login-tab">
-                    <li class="active">登录</li>
-                    <li>注册</li>
+                    <li @click="checkTab(true)" :class="{active:temp}">登录</li>
+                    <li @click="checkTab(false)" :class="{active:!temp}">注册</li>
                 </ul>
-                <div class="login-content">
+                <div class="login-content" v-if="temp">
                     <label>账号</label>
                     <div class="login-input">
                         <i class="user-pic"></i><input type="text" placeholder="输入用户名/邮箱"/>
@@ -25,7 +25,29 @@
                     <div class="login-input">
                         <i class="pwd-pic"></i><input type="text" placeholder="输入密码"/>
                     </div>
-                    <div class="drag">
+                    <div class="drag" ref="dragDiv">
+                        <div class="drag_bg"></div>
+                        <div class="drag_text">{{confirmWords}}</div>
+                        <div ref="moveDiv" @mousedown="mousedownFn($event)" :class="{'handler_ok_bg':confirmSuccess}" class="handler handler_bg" style="position: absolute;top: 0px;left: 0px;"></div>
+                    </div>
+                    <div class="login-radio">
+                        <span class="save"><input type="checkbox" />保存登录</span> 
+                        <span class="forget">忘记密码?</span>
+                    </div>
+                    <div class="save-btn">
+                        <a>立即登录<i></i></a>
+                    </div>
+                </div>
+                <div class="login-content" v-if="!temp">
+                    <label>账号1</label>
+                    <div class="login-input">
+                        <i class="user-pic"></i><input type="text" placeholder="输入用户名/邮箱"/>
+                    </div>
+                    <label>密码2</label>
+                    <div class="login-input">
+                        <i class="pwd-pic"></i><input type="text" placeholder="输入密码"/>
+                    </div>
+                    <div class="drag" ref="dragDiv">
                         <div class="drag_bg"></div>
                         <div class="drag_text">{{confirmWords}}</div>
                         <div ref="moveDiv" @mousedown="mousedownFn($event)" :class="{'handler_ok_bg':confirmSuccess}" class="handler handler_bg" style="position: absolute;top: 0px;left: 0px;"></div>
@@ -52,17 +74,20 @@
                 mouseMoveStata:false,     /*触发拖动状态  判断*/
                 maxwidth:'',               /*拖动最大宽度，依据滑块宽度算出来的*/
                 confirmWords:'拖动滑块验证',   /*滑块文字*/
-                confirmSuccess:false           /*验证成功判断*/
+                confirmSuccess:false,           /*验证成功判断*/
+                temp:true
             }
         },
         methods: {
+            //mousedoen 事件
             mousedownFn:function (e) {
                 if(!this.confirmSuccess){
                     e.preventDefault && e.preventDefault();   //阻止文字选中等 浏览器默认事件
                     this.mouseMoveStata = true;
                     this.beginClientX = e.clientX;
                 }
-            },        //mousedoen 事件
+            },
+            //验证成功函数        
             successFunction(){
                 this.confirmSuccess = true
                 this.confirmWords = '验证通过';
@@ -75,7 +100,8 @@
                 document.getElementsByClassName('drag_text')[0].style.color = '#fff'
                 document.getElementsByClassName('handler')[0].style.left = this.maxwidth + 'px';
                 document.getElementsByClassName('drag_bg')[0].style.width = this.maxwidth + 'px';
-            },                //验证成功函数
+            }, 
+            //mousemove事件               
             mouseMoveFn(e){
                 if(this.mouseMoveStata){
                     let width = e.clientX - this.beginClientX;
@@ -86,7 +112,8 @@
                         this.successFunction();
                     }
                 }
-            },                   //mousemove事件
+            }, 
+            //mouseup事件                 
             moseUpFn(e){
                 this.mouseMoveStata = false;
                 var width = e.clientX - this.beginClientX;
@@ -94,11 +121,15 @@
                     document.getElementsByClassName('handler')[0].style.left = 0 + 'px';
                     document.getElementsByClassName('drag_bg')[0].style.width = 0 + 'px';
                 }
-            }                       //mouseup事件
+            },
+            //tab切换                       
+            checkTab(t) {   
+                this.temp=t
+            }
         },
         mounted(){
-            // this.maxwidth = this.$refs.dragDiv.clientWidth - this.$refs.moveDiv.clientWidth;
-            this.maxwidth = 280;
+            this.maxwidth = this.$refs.dragDiv.clientWidth - this.$refs.moveDiv.clientWidth;
+            // this.maxwidth = 280;
             document.getElementsByTagName('html')[0].addEventListener('mousemove',this.mouseMoveFn);
             document.getElementsByTagName('html')[0].addEventListener('mouseup',this.moseUpFn)
         }
@@ -165,6 +196,7 @@
                     color: #909399;
                     margin-right: 30px;
                     padding-bottom: 10px;
+                    cursor: pointer;
                 }
                 li.active{
                     color: #000;
